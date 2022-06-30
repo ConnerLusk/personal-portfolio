eecd53f8d77573be03977b55e5d3aab142bb5760
ConnerLusk <90396333+ConnerLusk@users.noreply.github.com>
2022-06-29 18:26:35 -0700

    Update NavBar.jsx

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { HamburgerMenu } from "../../HamburgerMenu/index";
+import { HamburgerMenu } from "../hamburgerMenu/index";
 
 const NavbarContainer = styled.div`
   width: 100%;
