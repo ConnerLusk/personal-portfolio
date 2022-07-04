b44bc983fc7215adc03b6bb05353f0ca057aa9f8
ConnerLusk <lusk.c@northeastern.edu>
2022-07-04 11:18:50 -0700

    ga

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import {BrowserRouter as Router} from 'react-router-dom'
 import AnimatedRoutes from './Components/AnimatedRoutes';
+import ReactGa from 'react-ga'
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize('G-YD64K2DBMM')
+    // reprot page view
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
   return (
     <div className = "container">
           <Router>
