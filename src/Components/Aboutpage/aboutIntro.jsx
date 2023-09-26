5fa0de9c23206754c6f9d546ac2916f8985eb251
Conner Lusk <cjklusk@gmail.com>
2023-09-26 16:52:55 -0400

    add reprise

--- src/Components/Aboutpage/aboutIntro.jsx
+++ src/Components/Aboutpage/aboutIntro.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import './aboutIntro.css'
 
 function AboutIntro() {
-  return (
-    <div className='aboutIntroContainer'>
-        <div className='aboutPhotoContainer'>
-            <img src={require("./me.png")} alt="Me" width="400px" />
-        </div>
-        <div className='textContainer'>
-            <h1>Hello!</h1>
-            <div className='aboutTextContainer'>
-                <h2>
-                    I’m Conner Lusk, a third year computer science major at Northeastern University. 
-                    I am passionate about developing and researching new technology to improve the world we live in!
-                </h2>
-                <h2>
-                    Feel free to check out the projects and my previous work experience below.
-                </h2>
+    return (
+        <div className='aboutIntroContainer'>
+            <div className='aboutPhotoContainer'>
+                <img src={require("./me.png")} alt="Me" width="400px" />
+            </div>
+            <div className='textContainer'>
+                <h1>Hello!</h1>
+                <div className='aboutTextContainer'>
+                    <h2>
+                        I’m Conner Lusk, a fourth year computer science major at Northeastern University.
+                        I am passionate about developing and researching new technology to improve the world we live in!
+                    </h2>
+                    <h2>
+                        Feel free to check out the projects and my previous work experience below.
+                    </h2>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default AboutIntro
